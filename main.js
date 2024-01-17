c0187222d2573893abe0240dea4c85e841d8fd1d
SS4-Coder <89593962+SS4-Coder@users.noreply.github.com>
2024-01-17 20:06:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function bottle_pg(){
+    window.location="bottle.html";
+    }
+function pillows_pg(){
+    window.location="pillows.html"
+}
+function poloroid_pg(){
+    window.location="poloroid.html"
+}
+function sbag_pg(){
+    window.location="sbag.html"
+}
+function sunglasses_pg(){
+    window.location="sunglasses.html"
+}
+  
+  
